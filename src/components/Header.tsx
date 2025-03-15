9733f99ce9ce35bfccebb115226b8b5457cccd4b
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-03-15 20:30:34 -0300

    First Commit 🚀
    - Header adicionado
    - Botões de Neubrutalism adicionado

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import React from 'react'
+import NeoButton from './neo-button'
+
+const HeaderProps = [
+    { label: "Menu", href: "/" },
+    { label: "Templates", href: "/templates" },
+    { label: "Planos", href: "/planos" },
+    { label: "Descubra", href: "/descubra" },
+    { label: "Ajuda", href: "/ajuda" },
+]
+const Header = () => {
+    return (
+        <nav className='flex h-20 mx m-3 max-w-full items-center gap-4 p-4 rounded-lg bg-gray-200 shadow-black/30 shadow-md'>
+            <div>
+                <h1 className='font-bold italic text-xl'>Bionk</h1>
+            </div>
+            {HeaderProps.map((menu) => (
+                <ul className='' key={menu.label}>
+                    <li>
+                        <Link href={menu.href}>{menu.label}</Link>
+                    </li>
+                </ul>
+            )
+            )}
+            <div className='flex w-full justify-end gap-4'>
+                <NeoButton className='bg-white'>Login</NeoButton>
+                <NeoButton className='bg-green-400'>Cadastre-se</NeoButton>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/components/neo-button.tsx
@@ -0,0 +1,21 @@
+import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
+
+interface NeoButtonProps extends ComponentProps<'button'> {}
+
+const NeoButton = ({className, ...props}: NeoButtonProps) => {
+    return (
+      <button
+        className={'group/button rounded-lg bg-[#222222] text-black'}
+      >
+        <span
+          className={twMerge(
+            'block -translate-x-1 -translate-y-1 rounded-lg border-2 border-[#222222] bg-[#ff527a] px-4 py-1 text-sm font-medium tracking-tight transition-all group-hover/button:-translate-y-2 group-active/button:translate-x-0 group-active/button:translate-y-0 cursor-pointer', className)
+          }
+          {...props}
+        />
+      </button>
+    )
+  }
+  
+  export default NeoButton
