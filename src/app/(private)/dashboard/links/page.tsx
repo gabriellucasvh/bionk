2e15565b15ed5d1ee7c6e7c44b4122c74a81f9a3
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-03-19 01:49:23 -0300

    fix: margin bottom adicionada paras as páginas em dispositivos moveis

--- src/app/(private)/dashboard/links/page.tsx
+++ src/app/(private)/dashboard/links/page.tsx
@@ -5,7 +5,7 @@ export default function links() {
     return (
         <>
             <Sidebar />
-            <main className="ml-0 md:ml-64 h-screen overflow-y-auto">
+            <main className="ml-0 md:ml-64 mb-20 md:mb-0 h-screen overflow-y-auto">
                 <LinksClient />
             </main>
         </>
--- src/app/(private)/dashboard/perfil/page.tsx
+++ src/app/(private)/dashboard/perfil/page.tsx
@@ -6,7 +6,7 @@ export default function Perfil() {
     return (
         <>
             <Sidebar />
-            <main className="ml-0 md:ml-64 h-screen overflow-y-auto">
+            <main className="ml-0 md:ml-64 mb-20 md:mb-0 h-screen overflow-y-auto">
                 <PerfilClient />
             </main>
         </>
