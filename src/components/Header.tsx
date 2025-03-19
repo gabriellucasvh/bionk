cbd9ced75b17ac5a9d50019a2217e1101b8f2a0a
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-03-19 03:25:19 -0300

    feat: Botão de dashboard no Header para usuário logados

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,70 @@
 "use client"
 
-import Link from 'next/link'
 import React from 'react'
-import NeoButton from './neo-button'
+import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import NeoButton from './neo-button'
 
 const HeaderProps = [
-    { label: "Menu", href: "/" },
-    { label: "Templates", href: "/templates" },
-    { label: "Planos", href: "/planos" },
-    { label: "Descubra", href: "/descubra" },
-    { label: "Ajuda", href: "/ajuda" },
+  { label: "Menu", href: "/" },
+  { label: "Templates", href: "/templates" },
+  { label: "Planos", href: "/planos" },
+  { label: "Descubra", href: "/descubra" },
+  { label: "Ajuda", href: "/ajuda" },
 ]
-const Header = () => {
-    const route = useRouter()
-    return (
-        <nav className='hidden md:flex fixed inset-0 z-50 h-20 m-3 max-w-full items-center p-4 rounded-xl bg-white border'>
-            <div className='mx-10'>
-                <Link href={"/"}><Image src={"/bionk-logo.svg"} alt='logo' width={160} height={90} /></Link>
-            </div>
-            {HeaderProps.map((menu) => (
-                <ul className='' key={menu.label}>
-                    <li>
-                        <Link className='text-gray-600 hover:text-black transition-colors duration-200 px-3 py-2 rounded-md hover:bg-gray-200' href={menu.href}>{menu.label}</Link>
-                    </li>
-                </ul>
-            )
-            )}
-            <div className='flex w-full justify-end gap-4'>
-                <NeoButton onClick={() => route.push("/login")} className='bg-white'>Login</NeoButton>
-                <NeoButton onClick={() => route.push("/registro")} className='bg-green-400'>Cadastre-se</NeoButton>
-            </div>
-        </nav>
-    )
+
+const Header: React.FC = () => {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  return (
+    <nav className="hidden md:flex fixed inset-0 z-50 h-20 m-3 max-w-full items-center p-4 rounded-xl bg-white border">
+      <div className="mx-10">
+        <Link href="/">
+          <Image src="/bionk-logo.svg" alt="logo" width={160} height={90} />
+        </Link>
+      </div>
+      {HeaderProps.map((menu) => (
+        <ul key={menu.label}>
+          <li>
+            <Link
+              className="text-gray-600 hover:text-black transition-colors duration-200 px-3 py-2 rounded-md hover:bg-gray-200"
+              href={menu.href}
+            >
+              {menu.label}
+            </Link>
+          </li>
+        </ul>
+      ))}
+      <div className="flex w-full justify-end gap-4">
+        {session ? (
+          <NeoButton
+            onClick={() => router.push("/dashboard")}
+            className="bg-green-400"
+          >
+            Dashboard
+          </NeoButton>
+        ) : (
+          <div>
+            <NeoButton
+              onClick={() => router.push("/login")}
+              className="bg-white"
+            >
+              Entrar
+            </NeoButton>
+            <NeoButton
+              onClick={() => router.push("/registro")}
+              className="bg-green-400"
+            >
+              Cadastre-se
+            </NeoButton>
+          </div>
+        )}
+      </div>
+    </nav>
+  )
 }
 
 export default Header
--- src/components/HeaderMobile.tsx
+++ src/components/HeaderMobile.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import NeoButton from './neo-button';
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { Menu, X } from 'lucide-react';
 
@@ -16,6 +18,8 @@ const HeaderProps = [
 
 const HeaderMobile = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const { data: session } = useSession()
+    const router = useRouter()
 
     return (
         <nav className='flex md:hidden fixed inset-x-0 z-50 m-3 max-w-full items-center justify-between p-4 rounded-xl bg-white border'>
@@ -29,15 +33,15 @@ const HeaderMobile = () => {
             </button>
             <AnimatePresence>
                 {isOpen && (
-                    <motion.div 
-                        initial={{ opacity: 0, y: -10 }} 
+                    <motion.div
+                        initial={{ opacity: 0, y: -10 }}
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -10 }}
                         transition={{ duration: 0.3 }}
                         className='absolute top-full mt-1 rounded-xl left-0 w-full flex flex-col p-4 space-y-2 bg-white shadow-md border'>
                         {HeaderProps.map((menu) => (
-                            <motion.div 
-                                key={menu.label} 
+                            <motion.div
+                                key={menu.label}
                                 initial={{ opacity: 0, x: -10 }}
                                 animate={{ opacity: 1, x: 0 }}
                                 transition={{ delay: 0.1 }}
@@ -47,14 +51,37 @@ const HeaderMobile = () => {
                                 </Link>
                             </motion.div>
                         ))}
-                        <motion.div 
+                        <motion.div
                             initial={{ opacity: 0, y: 10 }}
                             animate={{ opacity: 1, y: 0 }}
                             transition={{ delay: 0.2 }}
                             className='flex flex-col gap-4 mt-4'
                         >
-                            <NeoButton className='bg-white'>Login</NeoButton>
-                            <NeoButton className='bg-green-400'>Cadastre-se</NeoButton>
+                            <div className="flex w-full justify-end gap-4">
+                                {session ? (
+                                    <NeoButton
+                                        onClick={() => router.push("/dashboard")}
+                                        className="bg-green-400"
+                                    >
+                                        Dashboard
+                                    </NeoButton>
+                                ) : (
+                                    <div>
+                                        <NeoButton
+                                            onClick={() => router.push("/login")}
+                                            className="bg-white"
+                                        >
+                                            Entrar
+                                        </NeoButton>
+                                        <NeoButton
+                                            onClick={() => router.push("/registro")}
+                                            className="bg-green-400"
+                                        >
+                                            Cadastre-se
+                                        </NeoButton>
+                                    </div>
+                                )}
+                            </div>
                         </motion.div>
                     </motion.div>
                 )}
