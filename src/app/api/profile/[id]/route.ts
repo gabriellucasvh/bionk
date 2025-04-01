6c5a1ae68db318d36e438b4483e3888b42d286b6
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-04-01 00:45:20 -0300

    fix: Rotas do next-auth agora estão distribuidas de maneira correta

--- src/app/api/profile/[id]/route.ts
+++ src/app/api/profile/[id]/route.ts
@@ -1,7 +1,7 @@
 // app/api/profile/[id]/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
