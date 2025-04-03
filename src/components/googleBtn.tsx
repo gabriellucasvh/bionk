1587dbf0f98eceed639192f2ebec9f0caf200fb0
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-04-03 17:45:39 -0300

    fix: googleBtn agora recebe o tipo button

--- src/components/googleBtn.tsx
+++ src/components/googleBtn.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 
 export function GoogleBtn() {
     return (
-        <button onClick={() => signIn('google', { callbackUrl: "/dashboard/perfil" })} className="flex items-center justify-center gap-2  w-full h-12 rounded-md cursor-pointer hover:bg-gray-200 border-2 transition-colors duration-300">
+        <button 
+        type="button"
+        onClick={() => signIn('google', { callbackUrl: "/dashboard/perfil" })}
+        className="flex items-center justify-center gap-2  w-full h-12 rounded-md cursor-pointer hover:bg-gray-200 border-2 transition-colors duration-300"
+        >
             <Image src={"/google-icon.png"} alt="Icone google" width={30} height={30} />
             Entrar com o Google
         </button>
