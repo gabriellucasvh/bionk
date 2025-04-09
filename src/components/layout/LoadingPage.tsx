c932b3238c9defced79350a75bd3959d108837b1
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-04-09 02:39:43 -0300

    style: Loading page agora cobre a largura toda

--- src/components/layout/LoadingPage.tsx
+++ src/components/layout/LoadingPage.tsx
@@ -1,6 +1,6 @@
 export default function LoadingPage() {
     return (
-        <section className="flex items-center justify-center h-screen">
+        <section className="flex items-center justify-center h-screen w-full">
             <span className="loader"></span>
         </section>
     );
