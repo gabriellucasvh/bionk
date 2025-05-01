669fc01a04c99b289c00842b715fe298ec6a0eb8
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-05-01 16:10:01 -0300

    fix(registro): Corrige a limitação de não usar unicodes

--- src/app/(public)/registro/formulario-registro.tsx
+++ src/app/(public)/registro/formulario-registro.tsx
@@ -17,9 +17,12 @@ import LoadingPage from "@/components/layout/LoadingPage";
 // Define the form schema
 const schema = z
   .object({
-    name: z.string().min(3, "Insira um nome de usuário válido").regex(/^[a-zA-Z0-9\s\-]+$/, {
-      message: "Use apenas letras, números e hífens",
-    }),
+    name: z.string()
+      .min(3, "Insira um nome de usuário válido")
+      .regex(/^[\p{L}0-9\s\-]+$/u, {
+        message: "Use apenas letras, números e hífens",
+      }),
+
     email: z.string().email("E-mail inválido"),
     password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
     confirmPassword: z.string(),
