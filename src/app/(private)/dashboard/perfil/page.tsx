ab23eb6ec038fdbb0e95ebb56fc690a126448989
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-05-11 23:15:32 -0300

    style: iframe removido da página de perfil

--- src/app/(private)/dashboard/perfil/page.tsx
+++ src/app/(private)/dashboard/perfil/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import Sidebar from '../dashboard-sidebar'
 import PerfilClient from './perfil.client'
-import IframePreview from '../IFramePreview'
 
 export const metadata: Metadata = {
   title: 'Bionk Perfil',
@@ -15,7 +14,6 @@ export default function Perfil() {
       <Sidebar />
       <main className="flex ml-0 md:ml-64 mb-20 md:mb-0 h-screen overflow-y-auto">
         <PerfilClient />
-        <IframePreview />
       </main>
     </>
   )
