a426d33bf93ba8c335c9d75ee933b94fb8a635da
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-05-12 16:54:28 -0300

    style(IFramePreview): atualizar largura e estilos de borda para melhor responsividade
    Ajustadas as cálculos de largura e a espessura da borda no componente `IFramePreview` para melhorar a responsividade em diferentes tamanhos de tela.

--- src/app/(private)/dashboard/IFramePreview.tsx
+++ src/app/(private)/dashboard/IFramePreview.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
@@ -24,7 +24,7 @@ export default function IframePreview() {
 
   return (
     <section className="fixed inset-0 flex items-center justify-center lg:items-end lg:justify-end z-30 pointer-events-none p-4 lg:p-8">
-      <div className="w-full max-w-[280px] md:max-w-[390px] aspect-[390/844] border-7 border-black rounded-4xl overflow-hidden bg-white shadow-lg pointer-events-auto">
+      <div className="w-[min(100%,calc(100vh*0.3))] sm:w-[min(100%,calc(100vh*0.5))] md:w-[min(100%,calc(100vh*0.45))] lg:w-[min(100%,calc(100vh*0.4))] xl:w-[min(100%,calc(100vh*0.43))] aspect-[390/844] border-4 border-black rounded-4xl overflow-hidden bg-white shadow-lg pointer-events-auto">
         <iframe
           src={iframeUrl || 'about:blank'}
           title="Pré-visualização do perfil"
