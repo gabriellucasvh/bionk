1b34f184f4a417f9d130efc3a01ba02455bbcc58
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-08-02 19:08:50 -0300

    style: adicionado metadata Open Graph em layout.tsx para exibir imagem, título e descrição ao compartilhar o link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,54 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Geist } from "next/font/google"
-import localFont from "next/font/local"
+import { Geist } from "next/font/google";
+import localFont from "next/font/local";
 import NextAuthSessionProvider from "@/providers/sessionProvider";
 
 const GeneralSans = localFont({
-  src: "/fonts/GeneralSans-Variable.woff2",
-  display: "swap",
-  variable: "--font-gsans"
-})
+	src: "/fonts/GeneralSans-Variable.woff2",
+	display: "swap",
+	variable: "--font-gsans",
+});
 const geist = Geist({
-  weight: ["100", "200", "300","400","500","600","700","800","900"],
-  subsets: ['latin'],
-  variable: '--font-geist',
-})
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ["latin"],
+	variable: "--font-geist",
+});
 
 export const metadata: Metadata = {
-  title: "Bionk",
-  description: "Crie o melhor organizador de links para o seu negócio",
-  manifest: "/manifest.json",
+	title: "Bionk",
+	description: "Reúna todos os seus links em um só lugar!",
+	manifest: "/manifest.json",
+	openGraph: {
+		title: "Bionk",
+		description: "Reúna todos os seus links em um só lugar!",
+		url: "https://www.bionk.me",
+		siteName: "Bionk",
+		images: [
+			{
+				url: "https://www.bionk.me/bionk-logo.png",
+				width: 1551,
+				height: 449,
+				alt: "Bionk",
+			},
+		],
+		type: "website",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR" className={`${geist.variable} ${GeneralSans.variable} antialiased`}>
-      <body>
-        <NextAuthSessionProvider>
-          {children}
-        </NextAuthSessionProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="pt-BR"
+			className={`${geist.variable} ${GeneralSans.variable} antialiased`}
+		>
+			<body>
+				<NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+			</body>
+		</html>
+	);
 }
