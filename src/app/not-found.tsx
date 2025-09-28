8b65c01cb805349e48de790f8d894793f823de3a
gabriellucasvh <gabriel.gopp@gmail.com>
2025-09-28 20:28:10 -0300

    style: replace min-h-screen with min-h-dvh for better viewport handling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function NotFound() {
 	return (
-		<div className="flex min-h-screen flex-col items-center justify-center bg-white">
-			<div className="flex min-h-screen flex-col items-center justify-center space-y-5 px-2">
+		<div className="flex min-h-dvh flex-col items-center justify-center bg-white">
+			<div className="flex min-h-dvh flex-col items-center justify-center space-y-5 px-2">
 				<Image alt="error" height={90} src={"/warning.svg"} width={160} />
 				<h2 className="font-bold text-5xl">Oops...</h2>
 				<p className="text-center">
