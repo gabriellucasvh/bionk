49cb304f79050d9fd20948f43af235ba4b1a3456
gabriellucasvh <gabriel.gopp@gmail.com>
2025-10-24 23:18:37 -0300

    feat: add audiomack domains to image config and update music options UI
    - Add new domains for audiomack image hosting to next.config.js
    - Update MusicOptions component with new audiomack icon styling
    - Improve soundcloud icon visibility with brightness and invert filters

--- next.config.js
+++ next.config.js
@@ -51,6 +51,26 @@ module.exports = {
 				hostname: "i1.sndcdn.com",
 				pathname: "/**",
 			},
+			{
+				protocol: "https",
+				hostname: "f4.bcbits.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "i.audiomack.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "images.audiomack.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "image-cdn.audiomack.com",
+				pathname: "/**",
+			},
 		],
 		qualities: [25, 50, 75, 90, 95, 100],
 	},
