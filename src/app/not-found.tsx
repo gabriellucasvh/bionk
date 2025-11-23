1165bc85c78e0dfa9937b7f33bb24dcdc7a8e8c7
gabriellucasvh <gabriel.gopp@gmail.com>
2025-11-23 20:03:22 -0300

    feat(not-found): update 404 page with new image and styling
    - Replace warning.svg with 404-foto.png for better visual representation
    - Add 'title' class to h2 element for consistent styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,8 @@ export default function NotFound() {
 	return (
 		<div className="flex min-h-dvh flex-col items-center justify-center bg-white">
 			<div className="flex min-h-dvh flex-col items-center justify-center space-y-5 px-2">
-				<Image alt="error" height={90} src={"/warning.svg"} width={160} />
-				<h2 className="font-bold text-5xl">Oops...</h2>
+				<Image alt="error" height={90} src={"/404-foto.png"} width={160} />
+				<h2 className="title font-bold text-5xl">Oops...</h2>
 				<p className="text-center">
 					Parece que temos algum problema aqui! A página foi movida ou não
 					existe mais.
